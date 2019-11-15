b035e8d6b46dcd50d694766d46e30dcb56b3b081
ankith88 <ankith88@gmail.com>
2019-11-16 08:27:09 +1100

    Initial commit

---
+++ mp_cl_create_user_note.js
@@ -0,0 +1,96 @@
+var baseURL = 'https://1048144.app.netsuite.com';
+if (nlapiGetContext().getEnvironment() == "SANDBOX") {
+    baseURL = 'https://system.sandbox.netsuite.com';
+}
+
+var user = 0;
+var role = nlapiGetRole();
+
+if (role == 1000) {
+    zee = nlapiGetUser();
+} else if (role == 3) { //Administrator
+    zee = 6; //test
+} else if (role == 1032) { // System Support
+    zee = 425904; //test-AR
+}
+
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
+
+function showAlert(message) {
+    $('#alert').html('<button type="button" class="close">&times;</button>' + message);
+    $('#alert').show();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+    // $(window).scrollTop($('#alert').offset().top);
+}
+
+$(document).on('click', '#alert .close', function(e) {
+    $(this).parent().hide();
+});
+
+function pageInit() {
+    $('#alert').hide();
+}
+
+function pageInit() {
+    $('#alert').hide();
+}
+
+function saveRecord() {
+
+    var customer_id = parseInt(nlapiGetFieldValue('custpage_customer_id'));
+
+
+    if (nlapiGetFieldValue('custpage_cancel') == 'true') {
+        var customerRecord = nlapiLoadRecord('customer', customer_id);
+        var zee_id = customerRecord.getFieldValue('partner');
+        var companyName = customerRecord.getFieldValue('companyname');
+        var entityid = customerRecord.getFieldValue('entityid');
+
+        var reason = nlapiGetFieldValue('custpage_reason')
+
+        var partnerRecord = nlapiLoadRecord('partner', zee_id);
+        var zee_email = partnerRecord.getFieldValue('email');
+
+        if (isNullorEmpty($('#note').val())) {
+            //112209 Chloe Young
+            nlapiSendEmail(112209, zee_email, 'Cancellation: - ' + reason + ' - ' + entityid + ' ' + companyName, 'Customer Cancelled', 'raine.giderson@mailplus.com.au')
+        } else {
+            //112209 Chloe Young
+            nlapiSendEmail(112209, zee_email, 'Cancellation: - ' + reason + ' - ' + entityid + ' ' + companyName, $('#note').val(), 'raine.giderson@mailplus.com.au')
+        }
+
+
+
+    }
+
+    var userNoteRecord = nlapiCreateRecord('note');
+
+    userNoteRecord.setFieldValue('entity', customer_id);
+    userNoteRecord.setFieldValue('title', 'User Note');
+    userNoteRecord.setFieldValue('direction', $('#direction option:selected').val());
+    userNoteRecord.setFieldValue('notetype', $('#notetype option:selected').val());
+    userNoteRecord.setFieldValue('note', $('#note').val() + '\n\n Operator / Franchisee Notified');
+    userNoteRecord.setFieldValue('author', nlapiGetUser());
+    userNoteRecord.setFieldValue('notedate', getDate());
+
+
+
+    nlapiSubmitRecord(userNoteRecord);
+
+    return true;
+
+
+}
+
+function getDate() {
+    var date = new Date();
+    // if (date.getHours() > 6) {
+    //     date = nlapiAddDays(date, 1);
+    // }
+    date = nlapiDateToString(date);
+    return date;
+}
---
+++ mp_sl_create_user_note.js
@@ -0,0 +1,98 @@
+var baseURL = 'https://1048144.app.netsuite.com';
+if (nlapiGetContext().getEnvironment() == "SANDBOX") {
+	baseURL = 'https://system.sandbox.netsuite.com';
+}
+
+var zee = 0;
+var role = nlapiGetRole();
+
+if (role == 1000) {
+	zee = nlapiGetUser();
+} else if (role == 3) { //Administrator
+	zee = 6; //test
+} else if (role == 1032) { // System Support
+	zee = 425904; //test-AR
+}
+
+
+function userNote(request, response) {
+	if (request.getMethod() == "GET") {
+
+		var params = request.getParameter('params');
+		params = JSON.parse(params);
+		var customer_id = params.custid;
+		var reason = params.reason;
+		var script_id = params.id;
+		var deploy_id = params.deploy;
+		var type = params.type;
+		var cancel = null;
+		cancel = params.cancel;
+
+		var recCustomer = nlapiLoadRecord('customer', customer_id);
+
+		var form = nlapiCreateForm('Create User Note: <a href="' + baseURL + '/app/common/entity/custjob.nl?id=' + customer_id + '">' + recCustomer.getFieldValue('entityid') + '</a> ' + recCustomer.getFieldValue('companyname'));
+
+		form.addField('custpage_customer_id', 'integer', 'Customer ID').setDisplayType('hidden').setDefaultValue(customer_id);
+		form.addField('custpage_suitlet', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(script_id);
+		form.addField('custpage_reason', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(reason);
+		form.addField('custpage_deploy', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(deploy_id);
+		form.addField('custpage_type', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(type);
+		form.addField('custpage_cancel', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(cancel);
+
+		var inlineHtml = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script src="//code.jquery.com/jquery-1.11.0.min.js"></script><link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css"><script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script><link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"><script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script><link rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2060796&c=1048144&h=9ee6accfd476c9cae718&_xt=.css"/><script src="https://1048144.app.netsuite.com/core/media/media.nl?id=2060797&c=1048144&h=ef2cda20731d146b5e98&_xt=.js"></script><link type="text/css" rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2090583&c=1048144&h=a0ef6ac4e28f91203dfe&_xt=.css"><style>.mandatory{color:red;}</style>';
+
+		inlineHtml += '<div class="se-pre-con"></div><div class="container" style="padding-top: 3%;"><div id="alert" class="alert alert-danger fade in"></div>';
+
+		inlineHtml += '<div class="form-group container survey_section">';
+		inlineHtml += '<div class="row">';
+		inlineHtml += '<div class="col-xs-6 survey1"><div class="input-group"><span class="input-group-addon" id="survey1_text">DIRECTION </span><select id="direction" class="form-control direction" required><option></option>';
+		inlineHtml += '<option value="1">INCOMING</option>';
+		inlineHtml += '<option value="2">OUTGOING</option>';
+		inlineHtml += '</select></div></div>';
+		inlineHtml += '<div class="col-xs-6 notetype"><div class="input-group"><span class="input-group-addon" id="notetype_text">NOTE TYPE </span><select id="notetype" class="form-control notetype" required><option></option>';
+		inlineHtml += '<option value="3">EMAIL</option>';
+		inlineHtml += '<option value="7">NOTE</option>';
+		inlineHtml += '<option value="8">PHONE CALL</option>';
+		inlineHtml += '</select></div></div>';
+
+		inlineHtml += '</div>';
+		inlineHtml += '</div>';
+
+		inlineHtml += '<div class="form-group container note_section">';
+		inlineHtml += '<div class="row">';
+		inlineHtml += '<div class="col-xs-6 note"><div class="input-group"><span class="input-group-addon" id="note_text">NOTE </span><textarea id="note" class="form-control note" rows="4" cols="50"  /></textarea></div></div>';
+		inlineHtml += '</div>';
+		inlineHtml += '</div>';
+
+		form.addField('preview_table', 'inlinehtml', '').setLayoutType('outsidebelow', 'startrow').setDefaultValue(inlineHtml);
+
+
+		form.addSubmitButton('Submit');
+		form.addButton('back', 'Back', 'onclick_back()');
+		form.addButton('back', 'Reset', 'onclick_reset()');
+
+		form.setScript('customscript_cl_create_user_note');
+
+		response.writePage(form);
+	} else {
+
+		var customer_id = request.getParameter('custpage_customer_id');
+		var suitlet_id = request.getParameter('custpage_suitlet');
+		var deploy_id = request.getParameter('custpage_deploy');
+		var type = request.getParameter('custpage_type');
+		var cancel = request.getParameter('custpage_cancel');
+
+		if (!isNullorEmpty(cancel)) {
+			response.sendRedirect('RECORD', 'customer', parseInt(customer_id), false);
+		} else {
+			var params = {
+				custid: parseInt(customer_id),
+				type: type
+			}
+			nlapiSetRedirectURL('SUITELET', suitlet_id, deploy_id, null, params);
+		}
+
+
+	}
+
+}
