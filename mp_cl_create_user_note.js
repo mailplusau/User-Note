3c662f7d1d0c8c2d2bb10dcbd5e4118d44961b45
ankith88 <ankith88@gmail.com>
2022-01-19 10:27:42 +1100

    CU-1unh28y
    Removing email to RTA and just informing team about the cancellation that ha been process with the date

--- mp_cl_create_user_note.js
+++ mp_cl_create_user_note.js
@@ -1,117 +1,149 @@
+/**
+ * @Author: Ankith Ravindran <ankithravindran>
+ * @Date:   2021-09-15T17:00:25+10:00
+ * @Filename: mp_cl_create_user_note.js
+ * @Last modified by:   ankithravindran
+ * @Last modified time: 2022-01-19T09:59:31+11:00
+ */
+
+
+
 var baseURL = 'https://1048144.app.netsuite.com';
 if (nlapiGetContext().getEnvironment() == "SANDBOX") {
-    baseURL = 'https://system.sandbox.netsuite.com';
+  baseURL = 'https://system.sandbox.netsuite.com';
 }
 
 var user = 0;
 var role = nlapiGetRole();
 
 if (role == 1000) {
-    zee = nlapiGetUser();
+  zee = nlapiGetUser();
 } else if (role == 3) { //Administrator
-    zee = 6; //test
+  zee = 6; //test
 } else if (role == 1032) { // System Support
-    zee = 425904; //test-AR
+  zee = 425904; //test-AR
 }
 
 $(window).load(function() {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");;
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
 });
 
 function showAlert(message) {
-    $('#alert').html('<button type="button" class="close">&times;</button>' + message);
-    $('#alert').show();
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0;
-    // setTimeout(function() {
-    //     $("#alert .close").trigger('click');
-    // }, 100000);
-    // $(window).scrollTop($('#alert').offset().top);
+  $('#alert').html('<button type="button" class="close">&times;</button>' +
+    message);
+  $('#alert').show();
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+  // setTimeout(function() {
+  //     $("#alert .close").trigger('click');
+  // }, 100000);
+  // $(window).scrollTop($('#alert').offset().top);
 }
 
 $(document).on('click', '#alert .close', function(e) {
-    $(this).parent().hide();
+  $(this).parent().hide();
 });
 
 function pageInit() {
-    $('#alert').hide();
+  $('#alert').hide();
 }
 
 function pageInit() {
-    $('#alert').hide();
+  $('#alert').hide();
 }
 
 function saveRecord() {
 
-    var customer_id = parseInt(nlapiGetFieldValue('custpage_customer_id'));
+  var customer_id = parseInt(nlapiGetFieldValue('custpage_customer_id'));
 
-    var userNoteRecord = nlapiCreateRecord('note');
+  var userNoteRecord = nlapiCreateRecord('note');
 
-    if (nlapiGetFieldValue('custpage_cancel') == 'true') {
-        userNoteRecord.setFieldValue('title', 'Cancel');
-        var customerRecord = nlapiLoadRecord('customer', customer_id);
-        var zee_id = customerRecord.getFieldValue('partner');
-        // var zee_text = customerRecord.getFieldText('partner');
-        var companyName = customerRecord.getFieldValue('companyname');
-        var entityid = customerRecord.getFieldValue('entityid');
-        var cancellation_date = customerRecord.getFieldValue('custentity13');
+  if (nlapiGetFieldValue('custpage_cancel') == 'true') {
+    userNoteRecord.setFieldValue('title', 'Cancel');
+    var customerRecord = nlapiLoadRecord('customer', customer_id);
+    var zee_id = customerRecord.getFieldValue('partner');
+    // var zee_text = customerRecord.getFieldText('partner');
+    var companyName = customerRecord.getFieldValue('companyname');
+    var entityid = customerRecord.getFieldValue('entityid');
+    var cancellation_date = customerRecord.getFieldValue('custentity13');
 
-        var reason = nlapiGetFieldValue('custpage_reason')
+    var reason = nlapiGetFieldValue('custpage_reason')
 
-        var partnerRecord = nlapiLoadRecord('partner', zee_id);
-        var zee_email = partnerRecord.getFieldValue('email');
+    var partnerRecord = nlapiLoadRecord('partner', zee_id);
+    var zee_email = partnerRecord.getFieldValue('email');
 
-        if (isNullorEmpty($('#note').val())) {
-            //112209 Chloe Young
-            if (reason != 'Change of Entity') {
-                nlapiSendEmail(112209, zee_email, 'Cancellation: - ' + entityid + ' ' + companyName, 'Customer Cancelled effective ' + cancellation_date + '</br> If an early invoice needs to be raised for the above mentioned customer, please contact accounts </br>', ['raine.giderson@mailplus.com.au', 'rianne.mansell@mailplus.com.au']);
-            }
-        } else {
-            //112209 Chloe Young
-            if (reason != 'Change of Entity') {
-                nlapiSendEmail(112209, zee_email, 'Cancellation: - ' + entityid + ' ' + companyName + ' effective ' + cancellation_date, 'If an early invoice needs to be raised for the above mentioned customer, please contact accounts </br></br>' + $('#note').val(), ['raine.giderson@mailplus.com.au', 'rianne.mansell@mailplus.com.au']);
-            }
-        }
-
-        if (reason != 'Change of Entity' && reason != 'Territory Transfer' && cancellation_date <= getDate()) {
-            nlapiSendEmail(696992, ['mailplussupport@protechly.com'], 'Deactivate Customer', ' Customer NS ID: ' + customer_id + '</br> Customer: ' + entityid + ' ' + companyName + '</br> Customer Franchisee NS ID: ' + zee_id, ['raine.giderson@mailplus.com.au', 'ankith.ravindran@mailplus.com.au', 'rianne.mansell@mailplus.com.au'])
-        }
-
-
-
+    if (isNullorEmpty($('#note').val())) {
+      //112209 Chloe Young
+      if (reason != 'Change of Entity') {
+        nlapiSendEmail(112209, zee_email, 'Cancellation: - ' + entityid + ' ' +
+          companyName, 'Customer Cancelled effective ' + cancellation_date +
+          '</br> If an early invoice needs to be raised for the above mentioned customer, please contact accounts </br>', [
+            'raine.giderson@mailplus.com.au',
+            'rianne.mansell@mailplus.com.au',
+            'fiona.harrison@mailplus.com.au'
+          ]);
+      }
     } else {
-        if (isNullorEmpty($('#title').val())) {
-            userNoteRecord.setFieldValue('title', $('#title').val());
-        } else {
-            userNoteRecord.setFieldValue('title', 'User Note');
-        }
+      //112209 Chloe Young
+      if (reason != 'Change of Entity') {
+        nlapiSendEmail(112209, zee_email, 'Cancellation: - ' + entityid + ' ' +
+          companyName + ' effective ' + cancellation_date,
+          'If an early invoice needs to be raised for the above mentioned customer, please contact accounts </br></br>' +
+          $('#note').val(), ['raine.giderson@mailplus.com.au',
+            'rianne.mansell@mailplus.com.au',
+            'fiona.harrison@mailplus.com.au'
+          ]);
+      }
+    }
 
+    if (reason != 'Change of Entity' && reason != 'Territory Transfer') {
+      nlapiSendEmail(696992, [
+          'raine.giderson@mailplus.com.au',
+          'ankith.ravindran@mailplus.com.au',
+          'rianne.mansell@mailplus.com.au', 'fiona.harrison@mailplus.com.au'
+        ],
+        'Deactivate Customer', ' Customer NS ID: ' + customer_id +
+        '</br> Customer: ' + entityid + ' ' + companyName +
+        '</br> Customer Franchisee NS ID: ' + zee_id +
+        '</br> Cancellation Date: ' + cancellation_date)
     }
 
 
-    userNoteRecord.setFieldValue('entity', customer_id);
 
-    userNoteRecord.setFieldValue('direction', $('#direction option:selected').val());
-    userNoteRecord.setFieldValue('notetype', $('#notetype option:selected').val());
-    userNoteRecord.setFieldValue('note', $('#note').val() + '\n\n Operator / Franchisee Notified');
-    userNoteRecord.setFieldValue('author', nlapiGetUser());
-    userNoteRecord.setFieldValue('notedate', getDate());
+  } else {
+    if (isNullorEmpty($('#title').val())) {
+      userNoteRecord.setFieldValue('title', $('#title').val());
+    } else {
+      userNoteRecord.setFieldValue('title', 'User Note');
+    }
+
+  }
+
+
+  userNoteRecord.setFieldValue('entity', customer_id);
+
+  userNoteRecord.setFieldValue('direction', $('#direction option:selected').val());
+  userNoteRecord.setFieldValue('notetype', $('#notetype option:selected').val());
+  userNoteRecord.setFieldValue('note', $('#note').val() +
+    '\n\n Operator / Franchisee Notified');
+  userNoteRecord.setFieldValue('author', nlapiGetUser());
+  userNoteRecord.setFieldValue('notedate', getDate());
 
 
 
-    nlapiSubmitRecord(userNoteRecord);
+  nlapiSubmitRecord(userNoteRecord);
 
-    return true;
+  return true;
 
 
 }
 
 function getDate() {
-    var date = new Date();
-    // if (date.getHours() > 6) {
-    //     date = nlapiAddDays(date, 1);
-    // }
-    date = nlapiDateToString(date);
-    return date;
+  var date = new Date();
+  // if (date.getHours() > 6) {
+  //     date = nlapiAddDays(date, 1);
+  // }
+  date = nlapiDateToString(date);
+  return date;
 }
--- mp_sl_create_user_note.js
+++ mp_sl_create_user_note.js
@@ -1,3 +1,13 @@
+/**
+ * @Author: Ankith Ravindran <ankithravindran>
+ * @Date:   2020-08-07T09:21:13+10:00
+ * @Filename: mp_sl_create_user_note.js
+ * @Last modified by:   ankithravindran
+ * @Last modified time: 2022-01-19T09:59:36+11:00
+ */
+
+
+
 var baseURL = 'https://1048144.app.netsuite.com';
 if (nlapiGetContext().getEnvironment() == "SANDBOX") {
 	baseURL = 'https://system.sandbox.netsuite.com';
@@ -30,32 +40,45 @@ function userNote(request, response) {
 
 		var recCustomer = nlapiLoadRecord('customer', customer_id);
 
-		var form = nlapiCreateForm('Create User Note: <a href="' + baseURL + '/app/common/entity/custjob.nl?id=' + customer_id + '">' + recCustomer.getFieldValue('entityid') + '</a> ' + recCustomer.getFieldValue('companyname'));
+		var form = nlapiCreateForm('Create User Note: <a href="' + baseURL +
+			'/app/common/entity/custjob.nl?id=' + customer_id + '">' + recCustomer.getFieldValue(
+				'entityid') + '</a> ' + recCustomer.getFieldValue('companyname'));
 
-		form.addField('custpage_customer_id', 'integer', 'Customer ID').setDisplayType('hidden').setDefaultValue(customer_id);
-		form.addField('custpage_suitlet', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(script_id);
-		form.addField('custpage_reason', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(reason);
-		form.addField('custpage_deploy', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(deploy_id);
-		form.addField('custpage_type', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(type);
-		form.addField('custpage_cancel', 'textarea', 'Latitude').setDisplayType('hidden').setDefaultValue(cancel);
+		form.addField('custpage_customer_id', 'integer', 'Customer ID').setDisplayType(
+			'hidden').setDefaultValue(customer_id);
+		form.addField('custpage_suitlet', 'textarea', 'Latitude').setDisplayType(
+			'hidden').setDefaultValue(script_id);
+		form.addField('custpage_reason', 'textarea', 'Latitude').setDisplayType(
+			'hidden').setDefaultValue(reason);
+		form.addField('custpage_deploy', 'textarea', 'Latitude').setDisplayType(
+			'hidden').setDefaultValue(deploy_id);
+		form.addField('custpage_type', 'textarea', 'Latitude').setDisplayType(
+			'hidden').setDefaultValue(type);
+		form.addField('custpage_cancel', 'textarea', 'Latitude').setDisplayType(
+			'hidden').setDefaultValue(cancel);
 
-		var inlineHtml = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script src="//code.jquery.com/jquery-1.11.0.min.js"></script><link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css"><script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script><link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"><script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script><link rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2060796&c=1048144&h=9ee6accfd476c9cae718&_xt=.css"/><script src="https://1048144.app.netsuite.com/core/media/media.nl?id=2060797&c=1048144&h=ef2cda20731d146b5e98&_xt=.js"></script><link type="text/css" rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2090583&c=1048144&h=a0ef6ac4e28f91203dfe&_xt=.css"><style>.mandatory{color:red;}</style>';
+		var inlineHtml =
+			'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><script src="//code.jquery.com/jquery-1.11.0.min.js"></script><link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css"><script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script><link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"><script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script><link rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2060796&c=1048144&h=9ee6accfd476c9cae718&_xt=.css"/><script src="https://1048144.app.netsuite.com/core/media/media.nl?id=2060797&c=1048144&h=ef2cda20731d146b5e98&_xt=.js"></script><link type="text/css" rel="stylesheet" href="https://1048144.app.netsuite.com/core/media/media.nl?id=2090583&c=1048144&h=a0ef6ac4e28f91203dfe&_xt=.css"><style>.mandatory{color:red;}</style>';
 
-		inlineHtml += '<div class="se-pre-con"></div><div class="container" style="padding-top: 3%;"><div id="alert" class="alert alert-danger fade in"></div>';
+		inlineHtml +=
+			'<div class="se-pre-con"></div><div class="container" style="padding-top: 3%;"><div id="alert" class="alert alert-danger fade in"></div>';
 
 		inlineHtml += '<div class="form-group container title_section">';
 		inlineHtml += '<div class="row">';
-		inlineHtml += '<div class="col-xs-6 title"><div class="input-group"><span class="input-group-addon" id="title_text">TITLE </span><input type="text" id="title" class="form-control title" /></div></div>';
+		inlineHtml +=
+			'<div class="col-xs-6 title"><div class="input-group"><span class="input-group-addon" id="title_text">TITLE </span><input type="text" id="title" class="form-control title" /></div></div>';
 		inlineHtml += '</div>';
 		inlineHtml += '</div>';
 
 		inlineHtml += '<div class="form-group container survey_section">';
 		inlineHtml += '<div class="row">';
-		inlineHtml += '<div class="col-xs-6 survey1"><div class="input-group"><span class="input-group-addon" id="survey1_text">DIRECTION </span><select id="direction" class="form-control direction" required><option></option>';
+		inlineHtml +=
+			'<div class="col-xs-6 survey1"><div class="input-group"><span class="input-group-addon" id="survey1_text">DIRECTION </span><select id="direction" class="form-control direction" required><option></option>';
 		inlineHtml += '<option value="1">INCOMING</option>';
 		inlineHtml += '<option value="2">OUTGOING</option>';
 		inlineHtml += '</select></div></div>';
-		inlineHtml += '<div class="col-xs-6 notetype"><div class="input-group"><span class="input-group-addon" id="notetype_text">NOTE TYPE </span><select id="notetype" class="form-control notetype" required><option></option>';
+		inlineHtml +=
+			'<div class="col-xs-6 notetype"><div class="input-group"><span class="input-group-addon" id="notetype_text">NOTE TYPE </span><select id="notetype" class="form-control notetype" required><option></option>';
 		inlineHtml += '<option value="3">EMAIL</option>';
 		inlineHtml += '<option value="7">NOTE</option>';
 		inlineHtml += '<option value="8">PHONE CALL</option>';
@@ -66,11 +89,13 @@ function userNote(request, response) {
 
 		inlineHtml += '<div class="form-group container note_section">';
 		inlineHtml += '<div class="row">';
-		inlineHtml += '<div class="col-xs-6 note"><div class="input-group"><span class="input-group-addon" id="note_text">NOTE </span><textarea id="note" class="form-control note" rows="4" cols="50"  /></textarea></div></div>';
+		inlineHtml +=
+			'<div class="col-xs-6 note"><div class="input-group"><span class="input-group-addon" id="note_text">NOTE </span><textarea id="note" class="form-control note" rows="4" cols="50"  /></textarea></div></div>';
 		inlineHtml += '</div>';
 		inlineHtml += '</div>';
 
-		form.addField('preview_table', 'inlinehtml', '').setLayoutType('outsidebelow', 'startrow').setDefaultValue(inlineHtml);
+		form.addField('preview_table', 'inlinehtml', '').setLayoutType('outsidebelow',
+			'startrow').setDefaultValue(inlineHtml);
 
 
 		form.addSubmitButton('Submit');
